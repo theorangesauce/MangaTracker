9ce9f3301d3ced247baa3494ac1c25a9d689f36f
theorangesauce <nickbishop97@gmail.com>
2020-05-01 17:36:34 -0400

    Create __main__.py
    __main__.py takes two command-line arguments, -g/--gui and -c/--cli, which are mutually exclusive. -g starts the Qt GUI version of the program, and -c starts the CLI version of the program.
    
    If no command line options are found, the program starts the CLI version by default, though a config option is planned to let the user decide.

---
+++ __main__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""__main__.py
+
+Start mangatracker program using command line arguments
+Copyright 2020 by Nicholas Bishop
+
+"""
+
+import argparse
+import os
+from mangatracker import main
+from mangatracker_gui import gui_main
+
+def start_cli():
+    main()
+    exit()
+
+def start_gui():
+    gui_main()
+    exit()
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    parser = argparse.ArgumentParser(prog="MangaTracker", description="Track a manga collection.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-g", "--gui", action="store_true", help="Start GUI")
+    group.add_argument("-c", "--cli", action="store_true", help="Start CLI")
+
+    args = parser.parse_args()
+        
+    if args.gui:
+        start_gui()
+    if args.cli:
+        start_cli()
+
+    start_cli()
