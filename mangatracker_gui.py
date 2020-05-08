3468b2673b88f8b16a6967d142101492635d00d3
theorangesauce <nickbishop97@gmail.com>
2020-05-08 14:58:30 -0400

    Remove * import form QtWidgets
    Forgot to select line in previous commit

--- mangatracker_gui.py
+++ mangatracker_gui.py
@@ -2,7 +2,6 @@
 import sys
 import os.path
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import *
 from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtWidgets import QButtonGroup, QMenu, QActionGroup
 from PySide2.QtWidgets import QHeaderView, QTableWidgetItem, QListWidgetItem
